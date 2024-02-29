b141f2f6029ffe921536b6b55b949904cff530c6
dbwhdtjr0457 <dbwhdtjr0457@khu.ac.kr>
2024-02-29 18:36:15 +0900

    Feat: eslint, prettier 설치 및 설정 파일 작성
    - eslint 설치 및 eslintrc 작성
     - airbnb 규칙 적용
    - prettier 설치 및 prettierrc 작성

---
+++ FE/.eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+	env: {
+		browser: true,
+		es6: true,
+		node: true
+	},
+	extends: ['airbnb', 'plugin:prettier/recommended'],
+	rules: {
+		'prettier/prettier': 'error'
+	}
+};
