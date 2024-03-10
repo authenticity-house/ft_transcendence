75e3746ec1772d87d945b4e318b8091eb89cbb6a
jeongmin <yesuel1111@naver.com>
2024-03-10 18:15:33 +0900

    Refactor: 게임 로직 view 함수 수정
    - 현재 웹소켓에서만 게임 로직을 사용하고 view 함수가 사용되지 않음
    - 따라서 로직 삭제

--- GAME/match/views.py
+++ GAME/match/views.py
@@ -1,10 +1,5 @@
 from django.http import HttpResponse
-from .match_manager import MatchManager
 
 
 def start(request):
-    # 가로: 6, 세로: 4 의 화면에서 좌표를 화면 중앙으로 생각
-    match_manager = MatchManager()
-    match_manager.start_game()
-
     return HttpResponse("임시 반환값")
