0bd4996149777eb64779b77e954ff075c379c3ba
gemstoneyang <bennyws99@gmail.com>
2024-03-11 22:36:53 +0900

    Feat: 웹 소켓 종료 로직 구현
    - 게임이 시작되면 task 등록 후 game_session에 저장
    - 연결이 종료되어 disconnect 함수 호출 시 game_session을 종료

--- GAME/websocket/consumers.py
+++ GAME/websocket/consumers.py
@@ -8,6 +8,7 @@ class GameConsumer(AsyncWebsocketConsumer):
     def __init__(self, *args, **kwargs):
         super().__init__(args, kwargs)
         self.match_manager = None
+        self.game_session = None
 
     async def connect(self):
         await self.accept()
@@ -19,9 +20,19 @@ class GameConsumer(AsyncWebsocketConsumer):
         )
 
         self.match_manager = MatchManager(self)
-        asyncio.create_task(self.match_manager.start_game())
+        self.game_session = asyncio.create_task(self.match_manager.start_game())
 
     async def receive(self, text_data=None, bytes_data=None):
         text_data_json = json.loads(text_data)
         key_set = text_data_json["key_set"]
         await self.match_manager.local_move_paddles(key_set)
+
+    async def disconnect(self, code):
+        # code: 1000 정상 종료 1001 상대방이 떠남 1002 프로토콜 오류 (로깅 시 사용)
+        if self.game_session:
+            self.game_session.cancel()
+            try:
+                await self.game_session
+            except asyncio.CancelledError:
+                # 게임 세션 취소 성공
+                pass
