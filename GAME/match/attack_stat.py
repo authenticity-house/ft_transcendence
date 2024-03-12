1f76fa97dfeb77e814810c02726042789c6ebd62
jeongmin <yesuel1111@naver.com>
2024-03-12 20:34:10 +0900

    Feat: 공격성향을 확인하는 로직 변경
    - 패들을 4등분하여 위(1/4)와 아래(1/4)는 공격형, 중앙(1/2)는 방어형, 같으면 혼합형으로 반환

--- GAME/match/attack_stat.py
+++ GAME/match/attack_stat.py
@@ -1,46 +1,36 @@
 class AttackStat:
     def __init__(self, paddle_height: float) -> None:
         self._paddle_height: float = paddle_height
-        self._paddle_div_six: float = paddle_height / 6
+        self._paddle_div_four: float = paddle_height / 4
 
-        self._top: int = 0
-        self._center: int = 0
-        self._bottom: int = 0
+        self._offensive_style: int = 0
+        self._defensive_style: int = 0
 
         self._power_up: int = 0
 
     def get_attack_type(self) -> int:
         """0: 공격형 / 1: 방어형 / 2: 혼합형 중 하나를 반환"""
-
-        offensive_style_cnt = self._top + self._bottom
-        defensive_style_cnt = self._center
-
-        if defensive_style_cnt < offensive_style_cnt:
+        if self._defensive_style < self._offensive_style:
             return 0
-        if offensive_style_cnt < defensive_style_cnt:
+        if self._offensive_style < self._defensive_style:
             return 1
         return 2
 
     def update_pos_cnt(self, paddle_y: float, ball_y: float) -> None:
-        # 패들 상단 / 중앙 / 하단 부딪힌 경우 판단
-        if paddle_y + self._paddle_div_six < ball_y:
-            self.increase_top_pos()
-        elif paddle_y - self._paddle_div_six < ball_y:
-            self.increase_center_pos()
+        # 패들 상단(1/4) / 중앙(1/2) / 하단(1/4) 부딪힌 경우 판단
+        if paddle_y - self._paddle_div_four <= ball_y <= paddle_y + self._paddle_div_four:
+            self.increase_defensive_style()
         else:
-            self.increase_bottom_pos()
+            self.increase_offensive_style()
 
     def increase_power_up_cnt(self) -> None:
         self._power_up += 1
 
-    def increase_top_pos(self) -> None:
-        self._top += 1
+    def increase_offensive_style(self) -> None:
+        self._offensive_style += 1
 
-    def increase_center_pos(self) -> None:
-        self._center += 1
-
-    def increase_bottom_pos(self) -> None:
-        self._bottom += 1
+    def increase_defensive_style(self) -> None:
+        self._defensive_style += 1
 
     @property
     def power_up(self) -> int:
