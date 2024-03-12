2126d5a8946397a5e6dc1761e7dc066e53b8bd11
gemstoneyang <bennyws99@gmail.com>
2024-03-12 22:51:47 +0900

    Merge: 통계 브랜치와 merge

--- GAME/websocket/consumers.py
+++ GAME/websocket/consumers.py
@@ -19,15 +19,66 @@ class GameConsumer(AsyncWebsocketConsumer):
             )
         )
 
-        self.match_manager = MatchManager(self)
-        self.game_session = asyncio.create_task(self.match_manager.start_game())
-
     async def receive(self, text_data=None, bytes_data=None):
         msg = json.loads(text_data)
-        msg_type, msg_subtype, msg_data = msg["type"], msg["subtype"], msg["data"]
-        if msg_type == "game" and msg_subtype == "key_down":
-            key_set: set = set(msg_data["key_set"])
-            self.match_manager.keys = key_set
+        msg_type, msg_subtype, msg_data = msg["type"], msg["subtype"], msg["data"] # Todo: json에 해당 헤더가 없는 경우 예외 발생
+        if msg_type == "game":
+            if msg_subtype == "key_down":
+                key_set = msg_data["key_set"]
+                self.match_manager.keys = key_set
+            elif msg_subtype == "session_info": # 게임 초기 정보
+                print(msg_data["battle_mode"])
+                print(msg_data["total_score"])
+                print(msg_data["battle_mode"])
+                print(msg_data["level"])
+                print(msg_data["color"]["paddle"])
+                print(msg_data["color"]["background"])
+
+                self.match_manager = MatchManager(self)
+                self.game_session = asyncio.create_task(self.match_manager.start_game())
+
+                # await self.send(
+                #     text_data=json.dumps(
+                #         {
+                #             "type": "game",
+                #             "subtype": "match_init_setting",
+                #             "message": "",
+                #             "match_id": 123,
+                #             "data": {
+                #                 "battle_mode": 1,
+                #                 "color": {
+                #                     "paddle": "#FFFFFF",
+                #                     "background": "#FFFFFF",
+                #                     "ball": "#FFFFFF",
+                #                 },
+                #                 "ball": {
+                #                     "status": "in",
+                #                     "x": 0.0,
+                #                     "y": 0.0,
+                #                     "radius": 0.04,
+                #                 },
+                #                 "paddle1": {
+                #                     "x": -2.8,
+                #                     "y": 0.0,
+                #                     "width": 0.1,
+                #                     "height": 0.5,
+                #                 },
+                #                 "paddle2": {
+                #                     "x": 2.8,
+                #                     "y": 0.0,
+                #                     "width": 0.1,
+                #                     "height": 0.5,
+                #                 },
+                #                 "nickname": {
+                #                     "player1": "wonyang",
+                #                     "player2": "jeongmin"
+                #                 }
+                #             },
+                #         }
+                #     )
+                # )
+
+                pass
 
     async def disconnect(self, code):
         # code: 1000 정상 종료 1001 상대방이 떠남 1002 프로토콜 오류 (로깅 시 사용)
