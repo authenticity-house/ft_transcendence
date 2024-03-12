5e5f77ae490eb29462f7b4daa6fd9d60f5618cf6
irenee-14 <ireneee145@gmail.com>
2024-03-13 00:11:36 +0900

    Feat: 게임 설정 페이지 버튼 component를 이용하여 변경

--- FE/src/pages/GameSettingPage.js
+++ FE/src/pages/GameSettingPage.js
@@ -1,30 +1,45 @@
 import { changeUrl } from '../index.js';
 import HorizontalHeadCount from '../components/HorizontalHeadCount.js';
+import VerticalSmallButton from '../components/VerticalSmallButton.js';
 
 const html = String.raw;
 
 class GameSettingPage {
 	template() {
-		const horizontalHeadCount = new HorizontalHeadCount('1vs1', '토너먼트');
+		const horizonbuttonConfigs = [{ text: '1vs1' }, { text: '토너먼트' }];
+		const horizontalHeadCount = new HorizontalHeadCount(horizonbuttonConfigs);
+
+		const virticalbuttonConfigs = [
+			{ text: '세부설정', classes: 'head_blue_neon_15 blue_neon_10' },
+			{ text: '시작', classes: 'head_blue_neon_15 blue_neon_10' }
+		];
+		const verticalSmallButton = new VerticalSmallButton(virticalbuttonConfigs);
 
 		return html`
-			<div class="small-window head_white_neon_15">
+			<div class="game-setting-window head_white_neon_15">
 				<div class="game-setting-container">
 					<div class="game-setting-content-container">
 						<div>${horizontalHeadCount.template()}</div>
 						<div class="game-setting-nickname-container"></div>
 					</div>
+					<div class="verticalButton">${verticalSmallButton.template()}</div>
 				</div>
-				<button class="game-start-button head_blue_neon_15 blue_neon_10">
-					게임시작
-				</button>
 			</div>
 		`;
 	}
 
 	addEventListeners() {
-		const gameButton = document.querySelector('.game-start-button');
-		gameButton.addEventListener('click', () => {
+		const detailedButton = document.querySelector(
+			'.verticalButton button:first-child'
+		);
+		detailedButton.addEventListener('click', () => {
+			changeUrl('gameSettingDetailed');
+		});
+
+		const startButton = document.querySelector(
+			'.verticalButton button:nth-child(2)'
+		);
+		startButton.addEventListener('click', () => {
 			changeUrl('game');
 		});
 	}
