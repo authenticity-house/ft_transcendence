cf270e83cbd9537c6621cce7890dfea055be5eaf
jeongmin <yesuel1111@naver.com>
2024-03-12 18:50:16 +0900

    Feat: 매치가 끝난 후 통계 데이터 전송
    - 웹소켓 규약 맞춰서 전송
    - 구현되지 않은 값은 임시값으로 저장하여 전송

--- GAME/match/match_manager.py
+++ GAME/match/match_manager.py
@@ -1,6 +1,8 @@
 import asyncio
 import math
 import json
+import datetime as dt
+from django.utils import timezone
 from channels.generic.websocket import AsyncWebsocketConsumer
 
 from .player import Player
@@ -25,8 +27,16 @@ class MatchManager:
         self._ball: Ball = Ball()
         self._is_run = False
 
+        self._start_date: dt = timezone.now()
+        self._end_date: dt = None
+        self._last_scored_time: dt = None
+        self._rally_count_list: list = []
+
     async def start_game(self) -> None:
+        """매치 시작 후 1초당 60프레임으로 클라이언트에게 현재 상태 전송"""
         self.is_run = True
+
+        rally_cnt: int = 0
         while self.is_run:
             self.ball.move_pos()
 
@@ -38,45 +48,86 @@ class MatchManager:
             if self.player1.paddle.is_collides_with_ball(self.ball):
                 print("left --------- paddle reflect!")
                 self.bounce_ball_off_paddle(self.player1.paddle)
+                rally_cnt += 1
             if self.player2.paddle.is_collides_with_ball(self.ball):
                 print("right ---------- paddle reflect!")
                 self.bounce_ball_off_paddle(self.player2.paddle)
+                rally_cnt += 1
 
             # 오른쪽 득점
             if self.is_player2_scored():
                 print("player2 win!")
                 self.update_score(self.player2)
+                self._rally_count_list.append(rally_cnt)
+                rally_cnt = 0
                 self.reset()
 
             # 왼쪽 득점
             if self.is_player1_scored():
                 print("player1 win!")
                 self.update_score(self.player1)
+                self._rally_count_list.append(rally_cnt)
+                rally_cnt = 0
                 self.reset()
 
             if self.TOTAL_SCORE in (self.player1.score, self.player2.score):
-                self.end_game()
-
-            await self.socket.send(
-                text_data=json.dumps(
-                    {
-                        "ball": {"x": self.ball.x, "y": self.ball.y},
-                        "paddle1": {"x": self.player1.paddle.x, "y": self.player1.paddle.y},
-                        "paddle2": {"x": self.player2.paddle.x, "y": self.player2.paddle.y},
-                        "score": {"player1": self.player1.score, "player2": self.player2.score},
-                        "type": "data",
-                        "message": "data",
-                    }
+                await self.end_game()
+            else:
+                data = {
+                    "ball": {"status": "in", "x": self.ball.x, "y": self.ball.y},
+                    "paddle1": {"x": self.player1.paddle.x, "y": self.player1.paddle.y},
+                    "paddle2": {"x": self.player2.paddle.x, "y": self.player2.paddle.y},
+                    "score": {
+                        "player1": self.player1.score,
+                        "player2": self.player2.score,
+                        "latest": 1,
+                    },
+                }
+                await self.socket.send(
+                    text_data=json.dumps(
+                        {
+                            "type": "game",
+                            "subtype": "match_run",
+                            "message": "local match running!",
+                            "match_id": 1,
+                            "data": data,
+                        }
+                    )
                 )
-            )
             # FPS 설정 (60프레임)
             await asyncio.sleep(1 / 60)
 
-    def end_game(self) -> None:
+    async def end_game(self) -> None:
         self.is_run = False
+        self._end_date = timezone.now()
+
+        data = {
+            "date": self._start_date.strftime("%Y-%m-%d"),
+            "play_time": self.get_play_time(),
+            "rally": self.get_rally_cnt_stat(),
+            "max_ball_speed": self.ball.get_max_speed_stat(),
+            "player1": self.player1.get_match_stat(),
+            "player2": self.player2.get_match_stat(),
+            "graph": {},
+        }
+        print(data)
+
+        await self.socket.send(
+            text_data=json.dumps(
+                {
+                    "type": "game",
+                    "subtype": "match_end",
+                    "message": "local match end!",
+                    "match_id": 1,
+                    "data": data,
+                }
+            )
+        )
 
     def update_score(self, player) -> None:
         player.increase_score()
+        self._last_scored_time = timezone.now()
+        self.ball.update_max_speed_list()
 
     def is_ball_colliding_with_wall(self) -> bool:
         """벽 충돌 확인"""
@@ -117,6 +168,26 @@ class MatchManager:
                 self.player2.paddle.move_paddle_up()
             if key == "ArrowDown":
                 self.player2.paddle.move_paddle_down()
+
+    def get_play_time(self) -> str:
+        if self._end_date is None:
+            self._end_date = dt.datetime.now()
+
+        td: dt.timedelta = self._end_date - self._start_date
+
+        hours, remainder = divmod(td.seconds, 3600)
+        minutes, seconds = divmod(remainder, 60)
+
+        play_time = f"{hours:02}:{minutes:02}:{seconds:02}"
+        return play_time
+
+    def get_rally_cnt_stat(self) -> list:
+        """[최대, 평균, 최소] 공 최대 속도 리스트 반환"""
+
+        max_value, min_value = max(self._rally_count_list), min(self._rally_count_list)
+        avg_value = sum(self._rally_count_list) / len(self._rally_count_list)
+
+        return [max_value, avg_value, min_value]
 
     def reset(self):
         self.ball.reset()
