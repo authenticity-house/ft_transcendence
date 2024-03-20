c30cd92635b725894c0ef3d00fb22f132ecd2b44
jeongmin <yesuel1111@naver.com>
2024-03-21 04:02:52 +0900

    Feat: SessionManager 초기 형태 구현
    - GameConsumer에서 MatchManager가 아닌 SessionManager를 사용하도록 변경
    - 모든 분기를 SessionManager가 관리하는 데이터를 요청하는 방식
    - get_match_frame 함수는 yield from을 사용하여 값을 넘겨주도록 함

--- GAME/websocket/consumers.py
+++ GAME/websocket/consumers.py
@@ -4,15 +4,13 @@ from json.decoder import JSONDecodeError
 
 from channels.generic.websocket import AsyncWebsocketConsumer
 from channels.exceptions import StopConsumer
-from match.match_manager import MatchManager
-from match.player import Player
-from match.ball import Ball
+from session.session_manager import SessionManager
 
 
 class GameConsumer(AsyncWebsocketConsumer):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.match_manager = None
+        self.session_manager: SessionManager = None
         self.game_session = None
         self.connected = False
 
@@ -34,12 +32,34 @@ class GameConsumer(AsyncWebsocketConsumer):
 
     async def handle_message(self, msg):
         msg_type = msg.get("type")
+        msg_subtype = msg.get("subtype", "")
 
         if msg_type == "disconnect":
             await self.close(code=1000)
 
         elif msg_type == "game":
-            await self.handle_game_message(msg)
+            msg_data = msg.get("data", {})
+
+            if msg_subtype == "session_info":
+                await self.initialize_session(msg_data)
+            else:
+                await self.handle_game_message(msg)
+
+        elif msg_type == "game_over" and msg_subtype == "summary":
+            data = self.session_manager.get_summary_stat()
+            await self.send_message("summary", "6-4 최종 정보 전송", data, "game_over_response")
+
+    async def initialize_session(self, msg_data):
+        self.session_manager = SessionManager(msg_data)
+
+        battle_mode = self.session_manager.battle_mode
+
+        if battle_mode == 1:  # 1vs1
+            send_msg = self.session_manager.get_send_data("match_init_setting")
+            await self.send_message(*send_msg)
+        elif battle_mode == 2:  # 토너먼트
+            send_msg = self.session_manager.get_send_data("tournament_tree")
+            await self.send_message(*send_msg)
 
     async def handle_game_message(self, msg):
         msg_subtype = msg.get("subtype", "")
@@ -48,80 +68,32 @@ class GameConsumer(AsyncWebsocketConsumer):
         if msg_subtype == "key_down":
             try:
                 key_set = msg_data["key_set"]
-                self.match_manager.keys = set(key_set)
+                self.session_manager.set_match_key_set(key_set)
             except KeyError:
                 await self.send_error("key_set not provided")
-
-        elif msg_subtype == "session_info":
-            await self.initialize_session(msg_data)
 
         elif msg_subtype == "match_start":
             self.game_session = asyncio.create_task(self.run_game_session())
 
-    # 세션 초기화 로직
-    async def initialize_session(self, msg_data):
-        total_score = msg_data.get("total_score")
-        # msg_data["battle_mode"] 여기서 보고 토너먼트, 1대1 분기
+        else:
+            send_msg = self.session_manager.get_send_data(msg_subtype)
+            await self.send_message(*send_msg)
 
-        # 레벨 정보는 MatchManager가 받아서 알맞은 패들 크기, 공속도 설정하기
-        self.match_manager = MatchManager(total_score=total_score)
-        # 추가 설정 및 초기화 로직
-        await self.send_initial_settings(msg_data)
-
-    # 초기 설정 메시지 전송 로직
-    async def send_initial_settings(self, msg_data):
-        color_info = msg_data.get("color")
-        paddle_color = color_info["paddle"]
-        ball_color = color_info["ball"]
-        # 아직 공 색 정보는 클라이언트에게 안받음
-
-        ball: Ball = self.match_manager.ball
-        player1: Player = self.match_manager.player1
-        player2: Player = self.match_manager.player2
-
-        data = {
-            "battle_mode": msg_data.get("battle_mode"),
-            "color": {
-                "paddle": paddle_color,
-                "ball": ball_color,
-            },
-            "ball": {
-                "status": "in",
-                "x": ball.x,
-                "y": ball.y,
-                "radius": ball.radius,
-            },
-            "paddle1": {
-                "x": player1.paddle.x,
-                "y": player1.paddle.y,
-                "width": player1.paddle.width,
-                "height": player1.paddle.height,
-            },
-            "paddle2": {
-                "x": player2.paddle.x,
-                "y": player2.paddle.y,
-                "width": player2.paddle.width,
-                "height": player2.paddle.height,
-            },
-            "nickname": {
-                "player1": player1.name,
-                "player2": player2.name,
-            },
-        }
-        await self.send_message("match_init_setting", "", data)
+        # elif msg_subtype == "session_info":
+        #     await self.initialize_session(msg_data)
 
     async def run_game_session(self):
         """매치 시작 후 1초당 60프레임으로 클라이언트에게 현재 상태 전송"""
-        mm: MatchManager = self.match_manager
+        sm: SessionManager = self.session_manager
 
         # 매치 프레임 전송
-        for message in mm.get_match_frame():
+        for message in sm.get_match_frame():
             await self.send_message(*message)
             await asyncio.sleep(1 / 60)
 
         # 매치 통계 전송
-        data = mm.get_match_stat()
-        await self.send_message("match_end", "local match end!", data)
+        send_msg = sm.get_send_data("match_end")
+        await self.send_message(*send_msg)
 
     async def disconnect(self, code):
         self.connected = False
@@ -134,13 +106,13 @@ class GameConsumer(AsyncWebsocketConsumer):
                 pass  # 게임 세션 취소 성공
         await super().disconnect(code)
 
-    async def send_message(self, subtype, message, data=None):
+    async def send_message(self, subtype, message, data=None, msg_type="game"):
         if not self.connected:
             print("GameConsumer: WebSocket is not connected. Message not sent.")
             return
 
         msg = {
-            "type": "game",
+            "type": msg_type,
             "subtype": subtype,
             "message": message,
             "data": data or {},
