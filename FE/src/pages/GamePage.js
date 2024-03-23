3a44f7e9d29db85d471b27c79bafa31494792793
irenee-14 <ireneee145@gmail.com>
2024-03-23 21:52:17 +0900

    Feat: 부트스트랩 나가기 모달 UI

--- FE/src/pages/GamePage.js
+++ FE/src/pages/GamePage.js
@@ -35,7 +35,50 @@ class GamePage {
 				<div class="game-result display-medium48 pink_neon_10">Winner!</div>
 			</div>
 
+			<!-- 나가기 모달 -->
 			<button class="return-button" style="margin: 30px">Return</button>
+			<button
+				type="button"
+				class="btn btn-primary return-button"
+				data-bs-toggle="modal"
+				data-bs-target="#staticBackdrop"
+			>
+				나가기
+			</button>
+			<div
+				class="modal fade"
+				id="staticBackdrop"
+				data-bs-backdrop="static"
+				data-bs-keyboard="false"
+				tabindex="-1"
+				aria-labelledby="staticBackdropLabel"
+				aria-hidden="true"
+			>
+				<div class="modal-dialog modal-dialog-centered">
+					<div class="modal-content">
+						<div class="modal-header">
+							<h5 class="modal-title" id="staticBackdropLabel">Modal title</h5>
+							<button
+								type="button"
+								class="btn-close"
+								data-bs-dismiss="modal"
+								aria-label="Close"
+							></button>
+						</div>
+						<div class="modal-body"></div>
+						<div class="modal-footer">
+							<button type="button" class="btn btn-secondary">나가기</button>
+							<button
+								type="button"
+								class="btn btn-primary"
+								data-bs-dismiss="modal"
+							>
+								돌아가기
+							</button>
+						</div>
+					</div>
+				</div>
+			</div>
 		`;
 	}
 
