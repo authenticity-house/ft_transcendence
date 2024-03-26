0c4eec0240fd5a33d86c9219d433eb2da9b5e85f
gemstoneyang <bennyws99@gmail.com>
2024-03-26 18:44:12 +0900

    Feat: .env 통합하여 추가
    - compose에 .env 설정 추가
    - 백엔드와 게임서버의 .env 파일을 통합
    - favicon 추가

--- GAME/game_server/settings.py
+++ GAME/game_server/settings.py
@@ -29,11 +29,11 @@ environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env("SECRET_KEY")
+SECRET_KEY = env("GAME_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # False if not in os.environ because of casting above
-DEBUG = env("DEBUG")
+DEBUG = env("GAME_DEBUG")
 
 ALLOWED_HOSTS: List[str] = []
 
