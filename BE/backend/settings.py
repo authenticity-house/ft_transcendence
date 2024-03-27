5cf38a824ab3136fdebc2c423cc9de918938de02
jeongmin <yesuel1111@naver.com>
2024-03-27 20:54:16 +0900

    Chore: postgresql 연동
    - docker-compose.yml에 postgresql 컨테이너 설정 추가
    - settings.py 파일에 postgresql 설정 추가

--- BE/backend/settings.py
+++ BE/backend/settings.py
@@ -83,8 +83,12 @@ ASGI_APPLICATION = "backend.asgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("POSTGRES_DB"),
+        "USER": os.getenv("POSTGRES_USER"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
+        "HOST": os.getenv("POSTGRES_HOST"),
+        "PORT": os.getenv("POSTGRES_PORT"),
     }
 }
 
