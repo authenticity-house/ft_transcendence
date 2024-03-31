dbb620da4ffdc21f427fcc1e87fd4759bbdac870
jeongmin <yesuel1111@naver.com>
2024-03-31 17:09:17 +0900

    Feat: 유저 테이블 컬럼 조건 추가
    - REQUIRED_FIELDS 지정

--- BE/users/models.py
+++ BE/users/models.py
@@ -12,10 +12,17 @@ class Provider(models.TextChoices):
 class User(AbstractUser):
     """유저 테이블"""
 
-    nickname = models.CharField(max_length=20)
-    provider = models.CharField(max_length=4, choices=Provider.choices, default=Provider.PONG)
+    nickname = models.CharField(max_length=12, default="", null=False, unique=True)
+    provider = models.CharField(
+        max_length=4, choices=Provider.choices, default=Provider.PONG, null=False
+    )
     profile_url = models.CharField(max_length=250)
     date_updated = models.DateTimeField(auto_now=True)
 
     first_name = None
     last_name = None
+
+    REQUIRED_FIELDS = ["email", "nickname", "provider"]
+
+    def __str__(self):
+        return self.nickname
