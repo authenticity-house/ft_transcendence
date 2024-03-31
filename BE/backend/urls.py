16c970b2ed94b9d44185cf5f32c93a08bd39a8e1
gemstoneyang <bennyws99@gmail.com>
2024-03-31 22:08:59 +0900

    Feat: 유저 관리 api 경로 변경

--- BE/backend/urls.py
+++ BE/backend/urls.py
@@ -21,6 +21,4 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("users/", include("users.urls")),
-    path("dj-rest-auth/", include("dj_rest_auth.urls")),
-    path("dj-rest-auth/registration/", include("dj_rest_auth.registration.urls")),
 ]
--- BE/users/urls.py
+++ BE/users/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 
 urlpatterns = [
-    path("api-auth/", include("rest_framework.urls")),
+    path("", include("dj_rest_auth.urls")),
+    path("registration/", include("dj_rest_auth.registration.urls")),
 ]
