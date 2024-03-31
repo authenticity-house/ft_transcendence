a8881cdb9e1740742be26cefdb24d6c9b500c840
jeongmin <yesuel1111@naver.com>
2024-03-31 17:14:30 +0900

    Feat: dj-rest-auth, allauth를 사용한 회원가입 구현
    - 이메일 인증은 막아둠
    - CustomUserAccountAdapter 구현
      - 닉네임 필드를 함께 저장
    - UserSerializer 구현
    - CustomRegisterSerializer 구현

--- BE/backend/urls.py
+++ BE/backend/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("users/", include("users.urls")),
+    path("dj-rest-auth/", include("dj_rest_auth.urls")),
+    path("dj-rest-auth/registration/", include("dj_rest_auth.registration.urls")),
 ]
---
+++ BE/users/serializers.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers
+from dj_rest_auth.registration.serializers import RegisterSerializer
+
+from .models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    def create(self, validated_data) -> User:
+        user = User.objects.create_user(**validated_data)
+        return user
+
+    class Meta:
+        model = User
+        fields = ["password", "username", "email", "nickname"]
+
+
+class CustomRegisterSerializer(RegisterSerializer):
+    nickname = serializers.CharField(min_length=1, max_length=12)
+
+    def custom_signup(self, request, user):
+        user.nickname = self.data.get("nickname")
+        user.save()
