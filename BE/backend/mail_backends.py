56d86276a662d12836a0f9c3ea26a0c2fe21dcf5
gemstoneyang <bennyws99@gmail.com>
2024-04-02 20:44:00 +0900

    Feat: 이메일 전송 구현
    - SSL 인증서를 발급받지 않은 상태에서 요청을 보내도록 설정
    - 아직 메일 내 링크 클릭시 인증이 완료되는 기능은 미구현

---
+++ BE/backend/mail_backends.py
@@ -0,0 +1,22 @@
+from django.core.mail.backends.smtp import EmailBackend as BaseEmailBackend
+import ssl
+import smtplib
+
+
+class CustomEmailBackend(BaseEmailBackend):
+    def open(self):
+        if not self.connection:
+            context = ssl._create_unverified_context()
+
+            if self.use_ssl:
+                self.connection = smtplib.SMTP_SSL(self.host, self.port, context=context)
+            else:
+                self.connection = smtplib.SMTP(self.host, self.port)
+
+            if self.use_tls:
+                self.connection.starttls(context=context)
+
+            if self.username and self.password:
+                self.connection.login(self.username, self.password)
+
+        return super().open()
