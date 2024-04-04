afb6db18d2d8e653d4d4d95610263ba38256e0bf
gemstoneyang <bennyws99@gmail.com>
2024-04-04 21:47:06 +0900

    Feat: 회원가입 시 세션 생성 제거
    - dj_rest_auth는 회원가입 시 세션 생성을 하게 되어 있음
    - RegisterView를 오버라이딩하여 회원가입 절차가 끝나고 세션을 강제 삭제하도록 수정
    - 인증 링크를 눌렀을 때 서버로 요청이 가도록 함

--- BE/users/views.py
+++ BE/users/views.py
@@ -1,9 +1,11 @@
 from django.http import HttpResponseRedirect
-from rest_framework.exceptions import NotFound
+from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
 from allauth.account.models import EmailConfirmation, EmailConfirmationHMAC
+from dj_rest_auth.registration.views import RegisterView
+from allauth.account.adapter import get_adapter
 
 
 class ConfirmEmailView(APIView):
@@ -13,7 +15,7 @@ class ConfirmEmailView(APIView):
         self.object = confirmation = self.get_object()
         confirmation.confirm(self.request)
         # A React Router Route will handle the failure scenario
-        return HttpResponseRedirect('/login/success/')
+        return HttpResponseRedirect('/')
 
     def get_object(self, queryset=None):
         key = self.kwargs['key']
@@ -32,3 +34,37 @@ class ConfirmEmailView(APIView):
         qs = EmailConfirmation.objects.all_valid()
         qs = qs.select_related("email_address__user")
         return qs
+
+
+class CustomRegisterView(RegisterView):
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        data = self.get_response_data(user)
+
+        # 세션 강제 제거
+        get_adapter().unstash_user(request)
+        get_adapter().unstash_verified_email(request)
+        request.session.pop('account_user', None)
+        request.session.pop('account_verified_email', None)
+
+        if data:
+            response = Response(
+                data,
+                status=status.HTTP_201_CREATED,
+                headers=headers,
+            )
+        else:
+            response = Response(status=status.HTTP_204_NO_CONTENT, headers=headers)
+
+        return response
+
+    def perform_create(self, serializer):
+        user = serializer.save(self.request)
+
+        # 이메일 인증 진행
+        from allauth.account.utils import send_email_confirmation
+        send_email_confirmation(self.request._request, user, signup=True)
+        return user
