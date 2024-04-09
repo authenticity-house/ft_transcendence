a40d470c933871b2ad353032e42e87d5c97eb1b9
jeongmin <yesuel1111@naver.com>
2024-04-09 23:04:42 +0900

    Feat: 회원가입에 필요한 필드 중복 검사 API 구현
    - GET /users/check/ 요청 처리
    - 이메일, 아이디, 닉네임을 쿼리 파라미터로 받을 수 있음
    - 쿼리 파라미터가 존재하지 않을 포함 시 400
    - 허용되지 않은 쿼리 파라미터 포함 시 400
    - 중복된 값이 존재할 경우 409
    - 모두 중복되지 않을 시 200

--- BE/users/urls.py
+++ BE/users/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include, re_path
-from .views import ConfirmEmailView, CustomRegisterView, FriendAPIView
+from .views import ConfirmEmailView, CustomRegisterView, FriendAPIView, CheckDuplicateAPIView
 
 urlpatterns = [
     path("", include("dj_rest_auth.urls")),
+    path("check/", CheckDuplicateAPIView.as_view(), name="check_field_duplicates"),
     # path("registration/", include("dj_rest_auth.registration.urls")),
     path("registration/", CustomRegisterView.as_view(), name="rest_register"),
     # 유저가 클릭한 이메일(=링크) 확인
