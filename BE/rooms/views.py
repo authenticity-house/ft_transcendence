3de8a740ba79ee47b6c5ba3d873026989d896a5e
gemstoneyang <bennyws99@gmail.com>
2024-04-10 17:32:13 +0900

    Feat: 로그인된 유저만 방 관련 요청을 할 수 있도록 수정
    - 로그인 세션이 있는 유저만 API를 호출할 수 있도록 수정

--- BE/rooms/views.py
+++ BE/rooms/views.py
@@ -1,11 +1,16 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.authentication import SessionAuthentication
+from rest_framework.permissions import IsAuthenticated
 from .serializers import RoomCreateSerializer, RoomListSerializer
 from .room_manager import RoomManager
 
 
 class RoomView(APIView):
+    authentication_classes = [SessionAuthentication]
+    permission_classes = [IsAuthenticated]
+
     def get(self, request):
         rooms = RoomManager.room_list()
         serializer = RoomListSerializer(rooms, many=True)
