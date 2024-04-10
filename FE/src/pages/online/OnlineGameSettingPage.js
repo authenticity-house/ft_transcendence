59efcf1ed2e3c6c9267f630427f3f2320ed236b6
irenee-14 <ireneee145@gmail.com>
2024-04-10 21:32:10 +0900

    Feat: 방 만들기 정보 백엔드로 보내기

--- FE/src/pages/online/OnlineGameSettingPage.js
+++ FE/src/pages/online/OnlineGameSettingPage.js
@@ -4,6 +4,7 @@ import VerticalButton from '../../components/VerticalButton.js';
 import { Gamewebsocket } from '../../websocket/Gamewebsocket.js';
 import ButtonBackArrow from '../../components/ButtonBackArrow.js';
 import { pongImage } from '../../components/pongImage.js';
+import { getCookie } from '../../utils/getCookie.js';
 
 const html = String.raw;
 
@@ -109,7 +110,36 @@ class GameSettingPage {
 			updateRoomName(newData);
 
 			console.log('넘기는 값', newData);
-			changeUrlData('waitingRoom', newData);
+			// ----------------------------------
+
+			const payload = JSON.stringify(newData);
+			const csrfToken = getCookie('csrftoken');
+			console.log('Form data:', payload);
+			fetch('http://127.0.0.1:8080/api/rooms/', {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+					'X-CSRFToken': csrfToken
+				},
+				body: payload
+			})
+				.then((res) => {
+					// 200 : OK
+					if (res.ok) {
+						if (res.status === 201) {
+							// 201 : Created
+
+							return res.json();
+						}
+						return res.json();
+					}
+					throw new Error('Error');
+				})
+				.then((data) => console.log(data))
+				.catch((error) => console.error('Error:', error));
+
+			// ----------------------------------
+			// changeUrlData('waitingRoom', newData);
 		});
 		const backButton = document.querySelector('.button-back-in-window');
 		backButton.addEventListener('click', () => {
