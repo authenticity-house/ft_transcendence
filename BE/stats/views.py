3ee488276f48947f1abf6c08b5fa980dfdefb396
jeongmin <yesuel1111@naver.com>
2024-04-13 15:59:28 +0900

    Feat: 경기기록 생성 API 구현
    - 입력받을 때 player1과 player2의 id(pk)와 매치 통계 데이터를 받고 있음
    - 레이팅은 현재 목데이터 사용중

--- BE/stats/views.py
+++ BE/stats/views.py
@@ -1,1 +1,14 @@
-# Create your views here.
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from stats.serializers import MatchSerializer
+
+
+class MatchAPIView(APIView):
+    def post(self, request):
+        serializer = MatchSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
