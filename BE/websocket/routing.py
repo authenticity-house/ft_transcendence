ae54632f383c6e8824b6a85b6abab4692ff03bf0
gemstoneyang <bennyws99@gmail.com>
2024-04-22 14:19:05 +0900

    Fix: 웹소켓 라우팅 규칙 수정
    - 라우팅 규칙이 위쪽부터 매칭돼 / 경로로 모두 매칭되는 오류 수정

--- BE/websocket/routing.py
+++ BE/websocket/routing.py
@@ -3,6 +3,6 @@ from .room_consumer import RoomConsumer
 from .default_consumer import DefaultConsumer
 
 websocket_urlpatterns = [
-    re_path(r"^.*$", DefaultConsumer.as_asgi()),
     re_path(r"^ws/room/(?P<room_number>\d+)/$", RoomConsumer.as_asgi()),
+    re_path(r"^.*$", DefaultConsumer.as_asgi()),
 ]
