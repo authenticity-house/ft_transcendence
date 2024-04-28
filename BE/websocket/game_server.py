f71426fbedaca2b20437574045a245ec7cb4c4ec
gemstoneyang <bennyws99@gmail.com>
2024-04-28 15:14:44 +0900

    Style: 코드 포맷 수정

--- BE/websocket/game_server.py
+++ BE/websocket/game_server.py
@@ -2,10 +2,8 @@ import requests
 
 
 def request_game_session(room_info):
-    url = 'http://game:8000/online/'
-    data = {'test_key': 'hello game!'}
-    headers = {
-        'Content-Type': 'application/json'
-    }
+    url = "http://game:8000/online/"
+    data = {"test_key": "hello game!"}
+    headers = {"Content-Type": "application/json"}
     response = requests.post(url, json=data, headers=headers).json()
     return response["url"]
