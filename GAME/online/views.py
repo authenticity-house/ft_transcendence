76c6e537b5a1ca43fd118fb9b93cddee184a3450
gemstoneyang <bennyws99@gmail.com>
2024-04-28 21:44:08 +0900

    Refactor: 임시 url 경로 수정

--- GAME/online/views.py
+++ GAME/online/views.py
@@ -3,6 +3,7 @@ from django.http import JsonResponse
 from django.views.decorators.http import require_http_methods
 from django.views.decorators.csrf import csrf_exempt
 
+
 @csrf_exempt
 @require_http_methods(["POST"])
 def TestView(request):
@@ -11,6 +12,8 @@ def TestView(request):
         data = json.loads(request.body)
         # 여기에서 데이터를 처리하는 로직을 추가하세요.
         # 예제에서는 입력된 데이터를 그대로 반환합니다.
-        return JsonResponse({'detail': 'Data processed successfully', 'url': "/online/123/"}, status=200)
+        return JsonResponse(
+            {"detail": "Data processed successfully", "url": "/online/oneonone/123/"}, status=200
+        )
     except Exception as e:
-        return JsonResponse({'error': str(e)}, status=400)
+        return JsonResponse({"error": str(e)}, status=400)
