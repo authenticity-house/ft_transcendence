3b179428d463f8e2d94908fbacf432e8431b2e2c
gemstoneyang <bennyws99@gmail.com>
2024-04-30 22:02:20 +0900

    Feat: 게임 세션 접속 구현
    - channels group에 참가하는 로직이 await이 없던 오류 수정
    - 연결이 완료되면 게임 세션에 참가
    - 게임 세션에 모든 참가자가 접속하면 match_init_setting 정보 전송

--- GAME/websocket/online_consumers.py
+++ GAME/websocket/online_consumers.py
@@ -1,5 +1,7 @@
 from channels.generic.websocket import AsyncJsonWebsocketConsumer
 
+from online.services import OnlineSessionManager
+
 
 class OnlineDuelConsumer(AsyncJsonWebsocketConsumer):
     def __init__(self, *args, **kwargs):
@@ -18,11 +20,12 @@ class OnlineDuelConsumer(AsyncJsonWebsocketConsumer):
 
         self.session_number = int(self.scope["url_route"]["kwargs"]["session_number"])
         self.session_group_name = f"session_{self.session_number}"
-
-        self.channel_layer.group_add(self.session_group_name, self.channel_name)
+        await self.channel_layer.group_add(self.session_group_name, self.channel_name)
 
         await self.accept()
         await self.send_message("connection_established", "You are now connected!")
+
+        await OnlineSessionManager.join_session(self.session_number, "test_user")
 
     async def send_message(self, subtype, message, data=None, msg_type="game"):
         msg = {
@@ -34,3 +37,12 @@ class OnlineDuelConsumer(AsyncJsonWebsocketConsumer):
         }
 
         await self.send_json(msg)
+
+    async def receive_json(self, content, **kwargs):
+        msg_type = content.get("type", "invalid")
+        msg_body = content.get("data", "")
+        print(msg_type)
+        print(msg_body)
+
+    async def match_init_setting(self, event):
+        await self.send_json(event["data"])
