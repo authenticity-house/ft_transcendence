0f7182a400a2ac61e4b46daadb2d2ed32582110c
irenee-14 <ireneee145@gmail.com>
2024-05-01 01:03:54 +0900

    Style: 주석제거

--- FE/src/pages/register/registerAPI.js
+++ FE/src/pages/register/registerAPI.js
@@ -1,48 +1,3 @@
-// import { formDataToJson } from '../../utils/formDataToJson.js';
-// import { showModal, hideModal } from '../../components/modal/modalUtils.js';
-// import apiEndpoints from '../../constants/apiConfig.js';
-
-// export function registerAPI(formData, reset) {
-// 	const payload = formDataToJson(formData);
-
-// 	showModal('registerLoadingModal');
-
-// 	fetch(apiEndpoints.REGISTRATION_URL, {
-// 		method: 'POST',
-// 		headers: {
-// 			'Content-Type': 'application/json'
-// 		},
-// 		body: payload
-// 	}).then((response) => {
-// 		let modalToShow = 'registerFailModal';
-
-// 		// 200 : OK
-// 		if (response.ok) {
-// 			if (response.status === 201) {
-// 				// 201 : Created
-// 				modalToShow = 'registerModal';
-// 				reset();
-// 			}
-// 		}
-// 		if (response.status === 400) {
-// 			// 비밀번호 다름, 아이디/닉네임/이메일 중복
-// 			console.log('res', response);
-// 			// if (
-// 			// 	response.non_field_errors &&
-// 			// 	response.non_field_errors[0] ===
-// 			// 		'The password is too similar to the username.'
-// 			// )
-// 			modalToShow = 'registerSimilarModal';
-// 		}
-// 		if (modalToShow) {
-// 			hideModal('registerLoadingModal', () => {
-// 				showModal(modalToShow);
-// 			});
-// 		}
-// 		return response.json();
-// 	});
-// }
-
 import { formDataToJson } from '../../utils/formDataToJson.js';
 import { showModal, hideModal } from '../../components/modal/modalUtils.js';
 import apiEndpoints from '../../constants/apiConfig.js';
@@ -70,7 +25,7 @@ export function registerAPI(formData, reset) {
 			if (ok) {
 				if (status === 201) {
 					modalToShow = 'registerModal';
-					reset(); // 폼을 리셋합니다.
+					reset();
 				}
 			} else if (status === 400 || status === 403) {
 				if (
