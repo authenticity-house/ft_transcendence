f835b0184f60827b248de586a05609d347be7583
jeongmin <yesuel1111@naver.com>
2024-05-01 18:33:14 +0900

    Feat: 사용자 게임 통계 모델 구현
    - play_time 변수들은 DurationField 활용
    - User 모델과 1:1 관계로 OneToOneField 활용

--- BE/stats/models.py
+++ BE/stats/models.py
@@ -1,3 +1,4 @@
+from datetime import timedelta
 from django.db import models
 
 from users.models import User
@@ -30,3 +31,25 @@ class Match(models.Model):
     player1_attack_type = models.CharField(max_length=5, choices=AttackType.choices, null=False)
     player2_attack_type = models.CharField(max_length=5, choices=AttackType.choices, null=False)
     create_date = models.DateTimeField(auto_now_add=True)
+
+
+class UserStat(models.Model):
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        primary_key=True,
+    )
+    wins_count = models.IntegerField(default=0)
+    losses_count = models.IntegerField(default=0)
+    rating = models.IntegerField(default=2000)
+    local_play_time = models.DurationField(default=timedelta(seconds=0))
+    online_play_time = models.DurationField(default=timedelta(seconds=0))
+    max_rating = models.IntegerField(default=0)
+    max_ball_speed = models.FloatField(default=0)
+    max_rally_cnt = models.IntegerField(default=0)
+    create_date = models.DateTimeField(auto_now_add=True)
+    update_date = models.DateTimeField(auto_now=True)
+
+    def save(self, *args, **kwargs):
+        self.max_rating = max(self.rating, self.max_rating)
+        super().save(*args, **kwargs)
