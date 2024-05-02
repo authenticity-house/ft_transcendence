396b45779f9f14dda47e92af088e7b202d561af7
gemstoneyang <bennyws99@gmail.com>
2024-05-02 19:11:17 +0900

    Feat: 토너먼트 접속 url 제공
    - /online 요청을 받는 view의 이름을 수정
    - 방 설정에 맞는 url를 반환하도록 변경

--- GAME/online/views.py
+++ GAME/online/views.py
@@ -9,15 +9,16 @@ from online.services import OnlineSessionManager
 
 @csrf_exempt
 @require_http_methods(["POST"])
-def online_oneonone_view(request):
+def online_game_view(request):
     try:
         # JSON 데이터 파싱
         data = json.loads(request.body)
         info = data["info"]
         session_number = OnlineSessionManager.add_session(info)
 
+        mode = "oneonone" if info["battle_mode"] == 1 else "tournament"
         return JsonResponse(
-            {"detail": "Data processed successfully", "url": f"/online/oneonone/{session_number}/"},
+            {"detail": "Data processed successfully", "url": f"/online/{mode}/{session_number}/"},
             status=200,
         )
     except Exception as e:  # pylint: disable=broad-exception-caught
