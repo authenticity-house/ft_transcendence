41f38d16e097d30130007169b10fbe107cb27e84
jeongmin <yesuel1111@naver.com>
2024-05-02 16:56:00 +0900

    Fix: profile_url 반환 시 pong user라면 ip와 port를 붙여서 반환
    - env에 SERVER_IP와 SERVER_PORT 추가

--- BE/users/serializers.py
+++ BE/users/serializers.py
@@ -1,7 +1,13 @@
 from rest_framework import serializers
 from dj_rest_auth.registration.serializers import RegisterSerializer
 
+from backend.settings import SERVER_IP, SERVER_PORT
+
 from .models import User
+
+
+def transform_profile_url(profile_url):
+    return f"http://{SERVER_IP}:{SERVER_PORT}{profile_url}"
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -32,9 +38,23 @@ class CustomUserDetailsSerializer(serializers.ModelSerializer):
         fields = ("pk", "username", "email", "nickname", "provider", "profile_url")
         read_only_fields = ("pk", "username", "provider")
 
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        print(data)
+        if data["provider"] == "PONG":
+            data['profile_url'] = transform_profile_url(data['profile_url'])
+        return data
+
 
 class UserProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ("pk", "nickname", "profile_url")
-        read_only_fields = ("pk", "nickname", "profile_url")
+        fields = ("pk", "nickname", "profile_url", "provider")
+        read_only_fields = ("pk", "nickname", "profile_url", "provider")
+
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        if data["provider"] == "PONG":
+            data['profile_url'] = transform_profile_url(data['profile_url'])
+        data.pop("provider")
+        return data
