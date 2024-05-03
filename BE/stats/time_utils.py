cb82c9da91cf5f7eb890f6223f2a06631871b4e2
jeongmin <yesuel1111@naver.com>
2024-05-03 19:00:46 +0900

    Feat: 형식 예외처리 추가

--- BE/stats/time_utils.py
+++ BE/stats/time_utils.py
@@ -1,13 +1,18 @@
 from datetime import timedelta
 
+from rest_framework.exceptions import ParseError
+
 
 def parse_timedelta(time_str):
-    parts = time_str.split(":")
-    if len(parts) == 3:
-        hours, minutes, seconds = map(int, parts)
-        return timedelta(hours=hours, minutes=minutes, seconds=seconds)
-    if len(parts) == 2:
-        hours, minutes = map(int, parts)
-        return timedelta(hours=hours, minutes=minutes)
+    try:
+        parts = time_str.split(":")
+        if len(parts) == 3:
+            hours, minutes, seconds = map(int, parts)
+            return timedelta(hours=hours, minutes=minutes, seconds=seconds)
+        if len(parts) == 2:
+            hours, minutes = map(int, parts)
+            return timedelta(hours=hours, minutes=minutes)
+    except Exception as e:  # pylint: disable=broad-exception-caught
+        raise ParseError(detail=str(e)) from e
 
-    return timedelta(seconds=0)
+    return None
